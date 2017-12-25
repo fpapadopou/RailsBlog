3db27ae1ffb5447075aa55eb0c2777096047e9ce
Fotis Papadopoulos <fpapadopou@gmail.com>
2017-12-25 12:02:05 +0200

    Added new resource type blogpost to the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  # Add a new resource type, blogpost, representing blog posts.
+  # Using the 'bin/rails routes' command translates the line below
+  # to a list of default REST routes.
+  resources :blogpost
+
   root 'index#index'
 end
