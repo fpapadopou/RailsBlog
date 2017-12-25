8ee5b480fbaad2785c61a59d52b6e5e51f251376
Fotis Papadopoulos <fpapadopou@gmail.com>
2017-12-25 18:17:48 +0200

    Moved to PostgreSQL database (Heroku compatibility)

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171225102220) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "blogposts", force: :cascade do |t|
     t.string "title"
     t.text "text"
