8af1b81936bcc6b209e0a66b7735b7e75ae999e2
Fotis Papadopoulos <fpapadopou@gmail.com>
2017-12-25 23:37:50 +0200

    Remove comments when a post is deleted

--- app/models/blogpost.rb
+++ app/models/blogpost.rb
@@ -2,7 +2,7 @@
 # Created using the builtin Rails command
 # `bin/rails generate model Blogpost title:string text:text`
 class Blogpost < ApplicationRecord
-  has_many :comments
+  has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { minimum: 5, maximum: 120 }
   validates :text, presence: true, length: { minimum: 15, maximum: 2500 }
 end
